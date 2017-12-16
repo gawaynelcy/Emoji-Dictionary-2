dc5ace2b1ec22e2494546c8f826f0267c3a34626
Unknown <gawaynelcy@gmail.com>
2017-12-16 22:26:45 +0800

    Created a TableView & Segue is working

---
+++ Emoji Dictionary 2/EmojiTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Dictionary 2
+//
+//  Created by Lau Chin Wei on 16/12/2017.
+//  Copyright © 2017 Yun. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    var emojis = ["😀","💩","🏎","🛰","🏟","🥜"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return emojis.count
+        
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+
+        cell.textLabel?.text = emojis[indexPath.row]
+        
+        return cell
+        
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "ourSegue", sender: nil)
+        
+    }
+   
+}
