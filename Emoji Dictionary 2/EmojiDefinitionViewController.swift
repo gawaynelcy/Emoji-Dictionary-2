cc34aa1fbaa778d19494cf42d410f864b34a0da4
Unknown <gawaynelcy@gmail.com>
2017-12-17 15:53:40 +0800

    Changed the line value of label
    value changed from 1 to 0 to display the whole string of long texts.

--- Emoji Dictionary 2/EmojiDefinitionViewController.swift
+++ Emoji Dictionary 2/EmojiDefinitionViewController.swift
@@ -12,6 +12,10 @@ class EmojiDefinitionViewController: UIViewController {
     
     @IBOutlet weak var emojiLabel: UILabel!
     @IBOutlet weak var emojiDefinitionLabel: UILabel!
+    @IBOutlet weak var emojiName: UILabel!
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var emoji = ""
     
@@ -19,39 +23,49 @@ class EmojiDefinitionViewController: UIViewController {
         super.viewDidLoad()
 
         emojiLabel.text = emoji
+        emojiDefinitionLabel.lineBreakMode = .byWordWrapping
+        emojiDefinitionLabel.numberOfLines = 0;
         
 // ["😀","💩","🏎","🛰","🏟","🥜","🎷","🇲🇾"]
         
         if emoji == "😀" {
             emojiDefinitionLabel.text = "A nice smile!"
+            emojiName.text = "Classic Smiley"
         }
         
         if emoji == "💩" {
             emojiDefinitionLabel.text = "A smelly poo poo by Irwyn!"
+            emojiName.text = "Poop"
         }
         
         if emoji == "🏎" {
             emojiDefinitionLabel.text = "A weird Swift's formula car!"
+            emojiName.text = "Race car"
         }
         
         if emoji == "🛰" {
-            emojiDefinitionLabel.text = "A little satellite to stalk you!"
+            emojiDefinitionLabel.text = "A little satellite to stalk you! And I am doing this to test the long string of texts! If it succeeded, I it will display the whole paragraph of text strings in the emojiDefinitionLabel."
+            emojiName.text = "Satellite"
         }
         
         if emoji == "🏟" {
             emojiDefinitionLabel.text = "A red stadium for sports!"
+            emojiName.text = "Stadium"
         }
         
         if emoji == "🥜" {
-            emojiDefinitionLabel.text = "A roasted groundnut!"
+            emojiDefinitionLabel.text = "A roasted ground nut!"
+            emojiName.text =  "Ground nut"
         }
         
         if emoji == "🎷" {
-            emojiDefinitionLabel.text = "A noisy saxaphone!"
+            emojiDefinitionLabel.text = "A noisy saxophone!"
+            emojiName.text = "Saxophone"
         }
         
         if emoji == "🇲🇾" {
             emojiDefinitionLabel.text = "A country I love very much!"
+            emojiName.text = "Malaysia"
         }
         
     }
